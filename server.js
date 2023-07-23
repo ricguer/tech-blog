4a74661cdb632e6db5ff3be9755ac25cbfd619e8
ricguer <rj.guerra@outlook.com>
2023-07-23 15:53:38 -0400

    Setup Sequelize Connection Using .env File

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const routes = require("./controllers");
-
 const express = require("express");
 const exphbs = require("express-handlebars");
+
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,8 +12,13 @@ const hbs = exphbs.create({});
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(routes);
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}!`);
+    });
 });
