e9a14ce442a59222bd4b5947d8d2e804dea62c1a
ricguer <rj.guerra@outlook.com>
2023-07-23 19:39:22 -0400

    Retrieve All Posts When Loading Home Page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,25 @@
 const router = require("express").Router();
+const { Post, User } = require("../models");
 
-// Get homepage
+// GET all posts for homepage
 router.get("/", async (req, res) => {
     try {
-        res.render("homepage");
-    }
+        const dbPostData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ["username"],
+                },
+            ],
+        });
+
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+
+        res.render("homepage", {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } 
     catch (err) {
         res.status(500).json(err);
     }
